c97225e0ff5f3fb8dfdbd6a9813abd43ab1962c5
Rohan Goel <rohangoel107@gmail.com>
2021-08-21 19:40:18 +0530

    all functionality added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,32 +1,53 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 export default function Item(props) {
-  const [dishquantity, setdishquantity] = useState(props.data.quantity);
-  const [amount, setamount] = useState(0)
+
+  
+  const index = props.data.index;
+  const products = props.data.products;
+  const setProducts = props.data.setProducts;
+  const element = props.data.element;
+
+  const [dishquantity, setDishquantity] = useState(0);
+  const [amount, setAmount] = useState(0)
+
+  useEffect(() => {
+    let updatedproductlist = [...products];
+    updatedproductlist[index] = {
+          description: element.description,
+          price: element.price,
+          quantity: dishquantity
+        }
+    setProducts(updatedproductlist)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dishquantity])
+
 
 
   function addItem() {
-    setdishquantity((prevdishquantity) => prevdishquantity + 1);
-    props.setsubTotal(props.subTotal + props.data.price);
-    setamount(amount + props.data.price)
+    setDishquantity((prevdishquantity) => prevdishquantity + 1);
+    props.data.setsubTotal(props.data.subTotal + props.data.element.price);
+    setAmount(amount + props.data.element.price)    
   }
   function removeItem() {
     if (dishquantity > 0) {
-      setdishquantity((prevdishquantity) => prevdishquantity - 1);
-      props.setsubTotal(props.subTotal - props.data.price);
-      setamount(amount - props.data.price)
+      setDishquantity((prevdishquantity) => prevdishquantity - 1);
+      props.data.setsubTotal(props.data.subTotal - props.data.element.price);
+      setAmount(amount - props.data.element.price)
     }
   }
 
 
 
   return (
-    <div>
-      Name: {props.data.description}
-      Price: {props.data.price} Quantity:
+    <>
+      <td>{props.data.element.description}</td>
+      <td>{props.data.element.price}</td>
+      
       <button onClick={removeItem}>-</button>
-      {dishquantity} <button onClick={addItem}>+</button>
-      Amount: {amount}
-    </div>
+      {dishquantity}
+      <button onClick={addItem}>+</button>
+      <td>{amount}</td>
+    </>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,16 @@
 import React,{useState} from "react";
 import MenuItems from "../menuItems";
 import Item from "./Item";
+import easyinvoice from 'easyinvoice';
+
+
 export default function Menu() {
 
   const [subTotal, setsubTotal] = useState(0)
-  const [tip, setTip] = useState(0)
+  const [tip, setTip] = useState(10)
   const [totalAmount, setTotalAmount] = useState(0)
+  const [products, setProducts] = useState([])
+
   function handleInputChange(e) {
     setTip(e.target.value);
   }
@@ -13,22 +18,93 @@ export default function Menu() {
   function calculate() {
     setTotalAmount(subTotal + subTotal*(tip/100))
   }
+  function addTipToProduct() {
+    const productsListafterTip = products.map(element => {
+      const newelement = element;
+      newelement["tax"] = tip;
+      return newelement;
+    })
+    setProducts(productsListafterTip);
+    //now add productsListafterTip to data of html file
 
+  }
+  async function createpdf() {
+    addTipToProduct();
+    let data = {
+      //"documentTitle": "RECEIPT", //Defaults to INVOICE
+      //"locale": "de-DE", //Defaults to en-US, used for number formatting (see docs)
+      "currency": "INR", //See documentation 'Locales and Currency' for more info
+      "taxNotation": "TIP", //or gst
+      "marginTop": 25,
+      "marginRight": 25,
+      "marginLeft": 25,
+      "marginBottom": 25,
+      "sender": {
+        "company": "Delhi Fast Food",
+        "address": "Sample Street 123",
+        "zip": "1234 AB",
+        "city": "New Delhi",
+        "country": "India"
+      },
+      "client": {
+        "company": "Client Corp",
+        "address": "Clientstreet 456",
+        "zip": "4567 CD",
+        "city": "Clientcity",
+        "country": "Clientcountry"
+      },
+      "invoiceNumber": "2021.0001",
+      "invoiceDate": "1.1.2021",
+      // "products": [
+      //   {
+      //     "description": "burger",
+      //     "tax": 5,
+      //     "price": 400,
+      //     "quantity": "2",
+      //   },
+      //   {
+      //     "quantity": "1",
+      //     "description": "pizza",
+      //     "price": 600,
+      //     "tax": 5,
+      //   }
+      // ],
+      "bottomNotice": "Thank You!!",
 
-  
+    };
+
+    //add products array to data
+    data["products"] = products;
+    console.log(data);
+
+    const result = await easyinvoice.createInvoice(data);                       
+    easyinvoice.download('myInvoice.pdf', result.pdf);
+
+  }
+  //subTotal={subTotal} setsubTotal={setsubTotal}
   return (
     <div>
-      {MenuItems.map((element) => (
-        <Item data={element} key={element.description} subTotal={subTotal} setsubTotal={setsubTotal}/>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Amount</th>
+        </tr>
+        {MenuItems.map((element, index) => (
+        <tr><Item key={index} data={{element,subTotal,setsubTotal,products,setProducts,index}} /></tr>
       ))}
-      SubTotal Amount: {subTotal}
-      <br />
+      </table>
+      
+      <>SubTotal Amount: {subTotal}</>
+      
       <label>
         Tip %
         <input type="number" value={tip} onChange={handleInputChange} />
       </label>
-      <br />
-      <button onClick={calculate}>Calculate</button>Total Amount: {totalAmount}
+      <button onClick={calculate} className="diplayblock">Calculate</button>Total Amount: {totalAmount}
+      
+      <button onClick={createpdf} className="diplayblock">Create PDF</button>
     </div>
   );
 }
